c8c542d8eed30a569caaf32f2193731ca9e70fae
Juris Raudulis <juris.raudulis@outlook.com>
2025-04-28 11:24:01 +0100

    Setting up project

---
+++ script.js
@@ -0,0 +1,11 @@
+const startBudget = document.getElementById('starting-budget');
+const remainingBalance = document.getElementById('remaining-balance');
+const startBudgetInput = document.getElementById('starting-budget-input');
+const addBudgetBtn = decodeURIComponent.getElementById('add-budget');
+const expenseNameInput = document.getElementById('expense-name');
+const expenseAmountInput = document.getElementById('expense-amount');
+const addExpenseBtn = document.getElementById('add-expense');
+
+
+
+
