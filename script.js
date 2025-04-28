14041639681377dd2936ccc8a02e65f5add60b98
Juris Raudulis <juris.raudulis@outlook.com>
2025-04-28 15:34:19 +0100

    Making progress with functionality

--- script.js
+++ script.js
@@ -1,11 +1,62 @@
-const startBudget = document.getElementById('starting-budget');
+const startBudgetUi = document.getElementById('starting-budget');
 const remainingBalance = document.getElementById('remaining-balance');
 const startBudgetInput = document.getElementById('starting-budget-input');
-const addBudgetBtn = decodeURIComponent.getElementById('add-budget');
+const addBudgetBtn = document.getElementById('add-budget');
 const expenseNameInput = document.getElementById('expense-name');
 const expenseAmountInput = document.getElementById('expense-amount');
 const addExpenseBtn = document.getElementById('add-expense');
 
+let startingBudget = 0;
+let expenses = [];
+
+function convertToCurrency(number) {
+    return number.toLocaleString("en-GB", {style:"currency", currency:"GBP"});
+}
+
+function addBudget() {
+    startingBudget = Number(startBudgetInput.value);
+
+    if( startingBudget <= 0 || isNaN(startingBudget) ) {
+        alert('Please enter your starting budget');
+    } else {
+        startBudgetUi.textContent = convertToCurrency(startingBudget);
+        remainingBalance.textContent = convertToCurrency(startingBudget);
+    }
+
+    startBudgetInput.value = '';
+    
+}
+
+function addExpenses () {
+    let expenseName = expenseNameInput.value;
+    let expenseAmount = Number(expenseAmountInput.value);
+
+    if( expenseName === '' || expenseAmount <= 0 || isNaN(expenseAmount) ) {
+        alert('Enter expense name and valid ammount');
+    } else {
+        let expenseObj = {
+            name: `${expenseName}`,
+            amount: `${expenseAmount}`
+        }
+
+        expenses.push(expenseObj);
+    }
+    updateExpenseList();
+
+    expenseNameInput.value = '';
+    expenseAmountInput.value = '';
+}
+
+function updateExpenseList() {
+    expenses.forEach((expense) =>{
+        console.log(expense.name, expense.amount);
+    })
+}
 
 
 
+
+
+
+addBudgetBtn.addEventListener('click', addBudget);
+addExpenseBtn.addEventListener('click', addExpenses);
